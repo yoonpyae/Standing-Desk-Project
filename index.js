c00a78e553f3af7ebc3a66c2106b58b8640cf92d
ypeain <yoonpyae26@gmail.com>
2024-08-13 09:22:29 +0630

    Add Project

---
+++ index.js
@@ -0,0 +1,48 @@
+window.addEventListener("load", function () {
+  setTimeout(function open(event) {
+    document.querySelector(".modelBox").style.display = "block";
+  }, 2000);
+});
+document.querySelector("#popup-close").addEventListener("click", function () {
+  document.querySelector(".modelBox").style.display = "none";
+});
+
+let availableKeywords = [
+  'Adjustable Height',
+  'Glass Standing Desk',
+  'Electric Standing Desk',
+  'Standing Desk with Drawer',
+  'Standing Desk',
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function () {
+  let result = [];
+  let input = inputBox.value;
+  if (input.length) {
+    result = availableKeywords.filter((keyword) => {
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+  }
+  display(result);
+
+  if (!result.length ) {
+    resultsBox.innerHTML = '';
+  }
+}
+
+function display(result) {
+  const content = result.map((list) => {
+    return "<li onclick=selectInput(this)>" + list + "</li>";
+  });
+
+  resultsBox.innerHTML = "<ul>" + content.join("") + "</ul>";
+}
+
+function selectInput(list) {
+  inputBox.value = list.innerHTML;
+  resultsBox.innerHTML = '';
+}
+
